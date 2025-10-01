58a0133e3c3cf4e46968a066405703feae767425
Mahshid <mahshojaei2001@gmail.com>
2025-10-01 15:17:01 -0800

    chore(ui): create basic page layout and components (no SSR/SSG yet)

--- pages/service/index.js
+++ pages/service/index.js
@@ -1,7 +1,14 @@
+import ServicesSection from '@/components/Service/ServicesSection'
 import React from 'react'
+import { useRouter } from 'next/router'
 
 export default function Service() {
+  const route=useRouter()
   return (
-    <div>Service</div>
+    <>
+      <h1 className='text-end p-2'>Home{route.pathname}</h1>
+      <ServicesSection/>
+    </>
+    
   )
 }
